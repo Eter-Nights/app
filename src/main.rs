7ec0b6174e1971dd8b291620603faefad2395f21
Eter-Nigths <likun7402@foxmail.com>
2025-11-13 20:52:16 +0800

    http

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod http_request;
+mod http_response;
+mod server;
+mod router;
+mod handler;
+
+use server::Server;
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080");
+
+    server.run();
 }
